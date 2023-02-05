cd7d635b4bac0cbfc137cbe6d54a68fc1bd372e4
Barkhayot Juraev <coderviewer@gmail.com>
2023-02-05 15:35:58 +0900

    update

--- app.py
+++ app.py
@@ -12,14 +12,12 @@ def get_audio(link):
     yt = YouTube(link)  
 
     try:
-        # yt.streams.filter(progressive = True,  file_extension = "mp4").first().download(output_path = "./", 
-        # filename = "mmmm.mp4")
         stream = yt.streams.filter(only_audio=True).first(). download(output_path = "./", filename = "eee.mp3")
     except:
         print("Some Error!")
     print('Task Completed!')
 
-bot = telebot.TeleBot("5649052898:AAH1-nFWu7z9geayRDMhHhrIZFSUbD7pVII")
+bot = telebot.TeleBot("5444195255:AAFdov1TjVD9YOHyeOn8dYIqgjqIGLlBdUs")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -28,7 +26,6 @@ def send_welcome(message):
 
 @bot.message_handler(content_types=['text'])
 def echo_all(message):
-    #if(message.text[:31] == "https://www.youtube.com/"):
         data = get_audio(message.text)
         if data == False:
             bot.reply_to(message, "The link that you have shared is on Privet Account! We have no Access !")
@@ -36,6 +33,5 @@ def echo_all(message):
             print(message.text)
             file = open("eee.mp3", "rb")
             bot.send_audio(message.chat.id, file, timeout=10000)
-    #else:
-	    #bot.reply_to(message, "Your Link is not valid! Please send Valid Link !")
-bot.infinity_polling(timeout=30)
+if __name__ == "__main__":
+    bot.infinity_polling()
